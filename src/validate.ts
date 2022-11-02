1c36bd80f21e505f1c873ed6ce7c534e8c8539e0
jh2oman <jacobwaterman@MacBook-Pro.local>
2022-11-01 18:05:43 -0700

    format

--- src/validate.ts
+++ src/validate.ts
@@ -7,7 +7,9 @@ export function validate<T extends ZodType>(obj: any, schema: T): void {
   } catch (err) {
     if (err instanceof ZodError) {
       throw new ResponseError(
-        `Error validating object with schema ${schema.description}. ${JSON.stringify(err.format()._errors)}`,
+        `Error validating object with schema ${
+          schema.description
+        }. ${JSON.stringify(err.format()._errors)}`,
         err,
       )
     }
